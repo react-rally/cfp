d3c842d4a49d8eb6970c651b2a1b2db421a92664
Leah Silber <leahsilber@gmail.com>
2014-11-23 17:18:04 -0800

    Fixes #32 and related CSS

--- app/helpers/organizer/participant_invitations_helper.rb
+++ app/helpers/organizer/participant_invitations_helper.rb
@@ -1,6 +1,6 @@
 module Organizer::ParticipantInvitationsHelper
   def new_participant_invitation_button
-    link_to 'Invite new participant', '#', class: 'btn btn-primary',
+    link_to 'Invite New User', '#', class: 'btn btn-primary',
       data: { toggle: 'modal', target: "#new-participant-invitation" },
       id: 'invite-new-participant'
   end
