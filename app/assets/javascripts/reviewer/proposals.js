955481992a4569cefbe7a15393284a91fae00495
Jamison Dance <jergason@gmail.com>
2016-04-30 13:04:51 -0600

    make next proposal button work for reviewers

--- app/assets/javascripts/reviewer/proposals.js
+++ app/assets/javascripts/reviewer/proposals.js
@@ -4,7 +4,17 @@ $(document).ready(function() {
   $.datepicker.setDefaults($.datepicker.regional['']);
 
   var oTable = cfpDataTable('#reviewer-proposals.datatable', [ 'number', null,
-    'number', 'text', 'text', 'text', 'number', 'text', 'text', null ]);
+    'number', 'text', 'text', 'text', 'number', 'text', 'text', null ],
+    {
+      stateSaveParams: function () {
+        var rows = $('[data-proposal-id]');
+        var uuids = [];
+        rows.each(function (i, row) {
+          uuids.push($(row).data('proposal-uuid'));
+        });
+        localStorage.proposal_uuid_table_order = JSON.stringify(uuids);
+      }
+    });
 
   $("#sort_reset").click(function(){
     oTable.fnSortNeutral();
@@ -32,5 +42,24 @@ $(document).ready(function() {
       }
     }
   });
+
+  // Replace next proposal link with valid proposal path
+  var next_link = $(".next-proposal");
+  if (next_link.length > 0) {
+    if (localStorage.proposal_uuid_table_order !== undefined) {
+      var proposal_uuids = JSON.parse(localStorage.proposal_uuid_table_order);
+      var current_index = proposal_uuids.indexOf(next_link.data("proposal-uuid"));
+      if (current_index + 1 < proposal_uuids.length) {
+        var next_uuid = proposal_uuids[current_index + 1];
+        var href = next_link.attr("href");
+        var new_href = href.replace("PLACEHOLDER", next_uuid);
+        next_link.attr("href", new_href);
+      } else {
+        next_link.remove();
+      }
+    } else {
+      next_link.remove();
+    }
+  }
 
 });
